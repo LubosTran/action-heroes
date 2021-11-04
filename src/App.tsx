7859629a381dced53d0fb37fdfdc7e161476d3a4
LubosTran <lubostranpmj@gmail.com>
2021-11-04 01:20:21 +0100

    use custom hook to fetch data and display it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import React, { FC } from "react";
 import "./styles/App.css";
-import Content, { Hero } from "./components/Content/Content";
+import Content from "./components/Content/Content";
+import useFetch from "./hooks/useFetch";
 
 const App: FC = () => {
-  const data = [] as Hero[];
-  return <Content data={data} />;
+  const {
+    response: data,
+    loading,
+    error,
+  } = useFetch("https://superhero-search.p.rapidapi.com/api/heroes", {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "superhero-search.p.rapidapi.com",
+      "x-rapidapi-key": "40327cfeaemsh6f17a39819d2af5p1b450fjsnba94d98add12",
+    },
+  });
+
+  return (
+    <>
+      {loading && <div>Loading...</div>}
+      {error && <div>Error.</div>}
+      {!loading && !error && <Content data={data} />}
+    </>
+  );
 };
 
 export default App;
